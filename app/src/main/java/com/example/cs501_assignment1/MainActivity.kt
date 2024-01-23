d8499b1e495962efde069e357b63028939d7140a
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-01-22 20:24:53 -0500

    Added code to update text to say hello world

--- app/src/main/java/com/example/cs501_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/cs501_assignment1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cs501_assignment1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     // This function will be called when the button is clicked
     fun onHelloWorldClick(view: View) {
-        // Add your code here to handle the button click
-        // For example, you can show a toast message
-        println("helloing world")
+        // Find the TextView by ID
+        val helloWorldText = findViewById<TextView>(R.id.hello_world_text)
+
+        // Change the text of the TextView when the Button is clicked
+        helloWorldText.text = "Hello, world!"
     }
 }
